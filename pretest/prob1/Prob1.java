1a343bbf3e27fab2efcf9ebdbfe8c878fc640068
Caden Warren <cwarre100@uafs.edu>
2026-08-21 09:46:33 -0500

    Finish prob 1 - Start 2

--- pretest/prob1/Prob1.java
+++ pretest/prob1/Prob1.java
@@ -3,7 +3,30 @@ package prob1;
 public class Prob1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		int[] arr = {4, 7, 2, 9, 1};
+		int maxDif = findMaxDifference(arr);
+		System.out.println(maxDif);
+	}
+	
+	public static int findMaxDifference(int[] numbers) {
+		if (numbers.length < 2) {
+			return 0;
+		}
+		
+		int maxDif = 0;
+		
+		for (int i = 0; i < numbers.length - 1; i++) {
+			int newDif = numbers[i] - numbers[i + 1];
+			if (newDif < 0) {
+				newDif*=-1;
+			}
+			if (newDif > maxDif) {
+				maxDif = newDif;
+			}
+		}
+		
+		return maxDif;
+		
 	}
 
 }
--- pretest/prob2/Prob2.java
+++ pretest/prob2/Prob2.java
@@ -1,4 +1,5 @@
 package prob2;
+import java.lang.Str
 
 public class Prob2 {
 
@@ -6,5 +7,44 @@ public class Prob2 {
 		
 
 	}
+	
+	public static int countVowelSequences(String str) {
+		int strLen = str.length();
+		
+		char prevChar;
+		boolean prevCharIsVowel;
+		
+		int vowelCount = 0;
+		for (int i = 0; i < strLen - 1; i++) {
+			if (!prevChar) {
+				prevChar = str[i];
+				prevCharIsVowel = checkVowel(prevChar);
+				continue;
+			}
+			
+			if (!prevCharIsVowel) {
+				prevChar = str[i];
+				prevCharIsVowel = checkVowel(prevChar);
+				continue;
+			}
+			
+			if (checkVowel(str[i])) {
+				vowelCount++;
+				prevChar = str[i];
+				prevCharIsVowel = checkVowel(prevChar);
+				continue;
+			}
+			
+		}
+	}
+	
+	private static boolean checkVowel(char c) {
+		if (c,toLower() == "a" || c.toLower() = "e" || c.toLower() = "i" || c.toLower() = "o" || c.toLower() = "u") {
+			return true;
+		}
+		return false;
+	}
+	
+	
 
 }
